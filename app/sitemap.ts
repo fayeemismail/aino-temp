4946ff201630112c30f42dfb6329775f7bc1992c
fayeemismail <faheemmuhammed703@gmail.com>
2026-04-23 16:27:32 +0530

    url inspection3

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -34,5 +34,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.9,
     },
+    {
+      url: `${baseUrl}/client`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
   ];
 }
